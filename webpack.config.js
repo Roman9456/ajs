54d05722419147e261834e989b526d1858d2e138
Roman <smolyakov_ra@mail.ru>
2024-03-21 19:51:57 +0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+    library: 'ajs',
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+    globalObject: 'this',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
+};
